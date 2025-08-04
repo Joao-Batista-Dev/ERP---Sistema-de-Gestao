4b566471c6d101181ebe30106878b25f9a8e2150
Joao Batista <joaobldss@gmail.com>
2025-08-04 20:56:42 -0300

    feat: Creating System Authentication with JWT

--- core/settings.py
+++ core/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # django rest framework
     'rest_framework',
+    'rest_framework_simplejwt',
     # app
+    'authentication',
     'erp',
 ]
 
@@ -124,3 +126,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
--- erp/views.py
+++ erp/views.py
@@ -1,23 +1,28 @@
 from rest_framework.viewsets import ModelViewSet
 from erp.models import Client, Product, Order, Invoice
 from erp.serializers import ClientSerializer, ProductSerializer, OrderSerializer, InvoiceSerializer
+from rest_framework.permissions import IsAuthenticated
 
 
 class ClientApiV1Views(ModelViewSet):
     queryset = Client.objects.all()
     serializer_class = ClientSerializer
+    permission_classes = [IsAuthenticated,]
 
 
 class ProductApiV1Views(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    permission_classes = [IsAuthenticated,]
 
 
 class OrderApiV1View(ModelViewSet):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
+    permission_classes = [IsAuthenticated,]
 
 
 class InvoiceApiV1View(ModelViewSet):
     queryset = Invoice.objects.all()
     serializer_class = InvoiceSerializer
+    permission_classes = [IsAuthenticated,]
