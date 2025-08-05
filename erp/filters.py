a420523b8655c1d853237a29cabe4513e3f18baf
Joao Batista <joaobldss@gmail.com>
2025-08-05 10:38:53 -0300

    fix: filter bug fix

--- erp/filters.py
+++ erp/filters.py
@@ -2,24 +2,37 @@ import django_filters
 from erp.models import Client, Product, Order, Invoice
 
 class ClientFilter(django_filters.FilterSet):
+    name = django_filters.CharFilter(lookup_expr='icontains')
+    email = django_filters.CharFilter(lookup_expr='icontains')
+    phone = django_filters.CharFilter(lookup_expr='icontains')
+    document = django_filters.CharFilter(lookup_expr='icontains')
+
     class Meta:
         model = Client
         fields = ['name', 'email', 'phone', 'document',]
 
 
 class ProductFilter(django_filters.FilterSet):
+    name = django_filters.CharFilter(lookup_expr='icontains')
+
     class Meta:
         model = Product
         fields = ['name',]
 
 
 class OrderFilters(django_filters.FilterSet):
+    client = django_filters.CharFilter(lookup_expr='icontains')
+    product = django_filters.CharFilter(lookup_expr='icontains')
+
     class Meta:
         model = Order
         fields = ['client', 'product',]
 
 
 class InvoiceFilter(django_filters.FilterSet):
+    order = django_filters.CharFilter(lookup_expr='icontains')
+    number = django_filters.CharFilter(lookup_expr='icontains')
+
     class Meta:
         model = Invoice
         fields = ['order', 'number',]
--- erp/views.py
+++ erp/views.py
@@ -3,6 +3,7 @@ from erp.models import Client, Product, Order, Invoice
 from erp.serializers import ClientSerializer, ProductSerializer, OrderSerializer, InvoiceSerializer
 from rest_framework.permissions import IsAuthenticated
 from django_filters.rest_framework import DjangoFilterBackend
+from erp.filters import ClientFilter, ProductFilter, OrderFilters, InvoiceFilter
 
 
 class ClientApiV1Views(ModelViewSet):
@@ -10,22 +11,28 @@ class ClientApiV1Views(ModelViewSet):
     serializer_class = ClientSerializer
     permission_classes = [IsAuthenticated,]
     filter_backends = [DjangoFilterBackend,]
-    filterset_fields = ['name', 'email', 'phone', 'document',]
+    filterset_class = ClientFilter
 
 
 class ProductApiV1Views(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     permission_classes = [IsAuthenticated,]
+    filter_backends = [DjangoFilterBackend,]
+    filterset_class = ProductFilter
 
 
 class OrderApiV1View(ModelViewSet):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
     permission_classes = [IsAuthenticated,]
+    filter_backends = [DjangoFilterBackend,]
+    filterset_class = OrderFilters
 
 
 class InvoiceApiV1View(ModelViewSet):
     queryset = Invoice.objects.all()
     serializer_class = InvoiceSerializer
     permission_classes = [IsAuthenticated,]
+    filter_backends = [DjangoFilterBackend,]
+    filterset_class = InvoiceFilter
